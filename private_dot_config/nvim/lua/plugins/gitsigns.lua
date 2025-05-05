9df3c09d8b6ab54140543201f387c89a9ac63b33
kakashigenz <nguyenminhquang2406@gmail.com>
2025-05-05 10:39:40 +0700

    add config toggle git blame

--- private_dot_config/nvim/lua/plugins/gitsigns.lua
+++ private_dot_config/nvim/lua/plugins/gitsigns.lua
@@ -5,6 +5,7 @@ return {
     require("gitsigns").setup({
       vim.keymap.set("n", "<leader>gs", ":Gitsigns stage_hunk<CR>", { noremap = true, silent = true }),
       vim.keymap.set("n", "<leader>gr", ":Gitsigns reset_hunk<CR>", { noremap = true, silent = true }),
+      vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", { noremap = true, silent = true }),
     })
   end
 }
